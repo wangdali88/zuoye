872ad36cc1e1f0d1ecbcc1f2db49cc68058a16de
wangdali88 <378128852@qq.com>
2019-01-04 12:50:36 +0800

    add shangchuan.py

--- shangchuan.py
+++ shangchuan.py
@@ -1,6 +1,11 @@
 from selenium  import webdriver
+import time
 
 driver = webdriver.Chrome()
 driver.get("http://www.baidu.com")
-driver.find_element_by_css_selector('span class="soutu-btn"').click()
-driver.find_element_by_css_selector('input type="file"').send_keys('C:\\Users\\Public\\Pictures\\Sample Pictures\\baxianhua.jpg')
+driver.find_element_by_css_selector('span [class="soutu-btn"]').click()
+driver.find_element_by_css_selector('input[type="file"]').send_keys('C:\\Users\\Public\\Pictures\\Sample Pictures\\baxianhua.jpg')
+
+time.sleep(10)
+
+driver.quit()
